26fca5ecf105cf42d9de50ba704cb569a0a2a818
Kardenvan <glushkovlad98@gmail.com>
2024-02-19 01:12:45 +0700

    Custom sliver fill remaining for cases when there is no available space for the sliver

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:playground_app/widgets/graph/graph.dart';
+import 'package:playground_app/render_objects/custom_fill_sliver_ro_screen.dart';
+
+import 'libraries/local_auth_button_screen.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(
@@ -23,42 +25,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Playground'),
-        ),
-        body: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const SizedBox(height: 32),
-              Container(
-                width: 300,
-                height: 200,
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.black,
-                    strokeAlign: BorderSide.strokeAlignOutside,
-                  ),
-                ),
-                child: const Graph(
-                  bottomLeft: Offset(3, 3),
-                  points: [
-                    Offset(2, 2),
-                    Offset(3, 3),
-                    Offset(4, 5),
-                    Offset(8, 3),
-                    Offset(9, 12),
-                  ],
-                  drawAxes: true,
-                ),
-              ),
-              const SizedBox(height: 16),
-            ],
-          ),
-        ),
-      ),
+      home: const CustomFillSliverRoScreen(),
     );
   }
 }
---
+++ lib/render_objects/custom_fill_sliver_ro_screen.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'dart:math' as math;
+
+class CustomFillSliverRoScreen extends StatelessWidget {
+  const CustomFillSliverRoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Colors.transparent,
+            pinned: true,
+            stretch: false,
+            primary: false,
+            expandedHeight: 250,
+            toolbarHeight: 0,
+            flexibleSpace: FlexibleSpaceBar(
+              collapseMode: CollapseMode.none,
+              expandedTitleScale: 1,
+              background: Container(
+                height: 200,
+                width: 300,
+                color: Colors.red,
+              ),
+            ),
+          ),
+          SliverAppBar(
+            pinned: true,
+            floating: true,
+            toolbarHeight: 112,
+            elevation: 0,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+            ),
+            backgroundColor: Colors.white,
+            automaticallyImplyLeading: false,
+            flexibleSpace: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const SizedBox(height: 16),
+                Container(
+                  color: Colors.blue,
+                  constraints: const BoxConstraints.expand(height: 80),
+                ),
+                const SizedBox(height: 16),
+              ],
+            ),
+          ),
+          SliverList.separated(
+            itemCount: 2,
+            itemBuilder: (_, __) => Container(
+              color: Colors.green,
+              constraints: const BoxConstraints.expand(height: 80),
+            ),
+            separatorBuilder: (_, __) => Container(
+              color: Colors.black,
+              constraints: const BoxConstraints.expand(height: 2),
+            ),
+          ),
+          const DecoratedSliver(
+            decoration: BoxDecoration(color: Colors.orange),
+            sliver: _CustomSliverFillRemainingAndOverscroll(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomSliverFillRemainingAndOverscroll
+    extends SingleChildRenderObjectWidget {
+  const _CustomSliverFillRemainingAndOverscroll({
+    super.child,
+  });
+
+  @override
+  _CustomRenderSliverFillRemainingAndOverscroll createRenderObject(
+          BuildContext context) =>
+      _CustomRenderSliverFillRemainingAndOverscroll();
+}
+
+class _CustomRenderSliverFillRemainingAndOverscroll
+    extends RenderSliverSingleBoxAdapter {
+  _CustomRenderSliverFillRemainingAndOverscroll({super.child});
+
+  @override
+  void performLayout() {
+    final SliverConstraints constraints = this.constraints;
+
+    double extent =
+        constraints.viewportMainAxisExtent - constraints.precedingScrollExtent;
+    double maxExtent =
+        constraints.remainingPaintExtent - math.min(constraints.overlap, 0.0);
+
+    if (child != null) {
+      final double childExtent;
+      switch (constraints.axis) {
+        case Axis.horizontal:
+          childExtent =
+              child!.getMaxIntrinsicWidth(constraints.crossAxisExtent);
+        case Axis.vertical:
+          childExtent =
+              child!.getMaxIntrinsicHeight(constraints.crossAxisExtent);
+      }
+
+      extent = math.max(extent, childExtent);
+      maxExtent = math.max(extent, maxExtent);
+      child!.layout(constraints.asBoxConstraints(
+          minExtent: extent, maxExtent: maxExtent));
+    }
+
+    assert(
+      extent.isFinite,
+      'The calculated extent for the child of SliverFillRemaining is not finite. '
+      'This can happen if the child is a scrollable, in which case, the '
+      'hasScrollBody property of SliverFillRemaining should not be set to '
+      'false.',
+    );
+
+    // Эта строка добавлена в стандартный рендер объект для того, чтобы экран не
+    // падал в случае когда элементы в скролле уходят за экран.
+    // В текущем юзкейсе ничего не ломает и позволяет удовлетворить дизайн
+    extent = math.max(0, extent);
+
+    final double paintedChildSize =
+        calculatePaintOffset(constraints, from: 0.0, to: extent);
+    assert(paintedChildSize.isFinite);
+    assert(paintedChildSize >= 0.0);
+
+    geometry = SliverGeometry(
+      scrollExtent: extent,
+      paintExtent: math.min(maxExtent, constraints.remainingPaintExtent),
+      maxPaintExtent: maxExtent,
+      hasVisualOverflow: extent > constraints.remainingPaintExtent ||
+          constraints.scrollOffset > 0.0,
+    );
+    if (child != null) {
+      setChildParentData(child!, constraints, geometry!);
+    }
+  }
+}
